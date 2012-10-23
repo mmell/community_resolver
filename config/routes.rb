2b7e9ca4df72fdf0d818d3a7fa2fd82e5d569151
mmell <mike.mell@nthwave.net>
2012-10-23 16:05:07 -0700

    Update config/routes.rb
    format false allows dots in inames

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ require 'community_resolver'
 Rails.application.routes.draw do
   match "/:action/*xri", :to => ResolveController.action(:respond), :constraints => {
     :action => /#{CommunityResolver::Path::Authority}|#{CommunityResolver::Path::Proxy}/
-  }
+  }, :format => false # format false allows dots in inames
 end
