a2db8aa20f9372cd763fbb3b5cb57797a1acffbf
Michael Mell <mike.mell@nthwave.net>
2013-04-26 16:04:06 -0700

    fix routes for rails4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 require 'community_resolver'
 
 Rails.application.routes.draw do
-  match "/:action/*xri", :to => ResolveController.action(:respond), :constraints => {
-    :action => /#{CommunityResolver::Path::Authority}|#{CommunityResolver::Path::Proxy}/
-  }
+  match "/:action/*xri", 
+    :to => ResolveController.action(:respond), 
+    :constraints => {
+      :action => /#{CommunityResolver::Path::Authority}|#{CommunityResolver::Path::Proxy}/
+    },
+    :via => [:get, :post]
 end
